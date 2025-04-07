7fabf39bec8fd6fc3454db130a90dc5c2e53bf15
RulerOfCakes <fragmentbombs@gmail.com>
2025-04-07 18:38:01 +0900

    feat: data manipulation & CA rules

---
+++ src/model/data.rs
@@ -0,0 +1,143 @@
+use burn::{
+    data::dataloader::batcher::Batcher,
+    prelude::Backend,
+    tensor::{Tensor, TensorData},
+};
+
+#[derive(Clone)]
+pub struct CABatcher<B: Backend> {
+    device: B::Device,
+}
+
+impl<B: Backend> CABatcher<B> {
+    pub fn new(device: B::Device) -> Self {
+        CABatcher { device }
+    }
+}
+
+// Burn expects the batch to be in `NCHW` format.
+#[derive(Clone, Debug)]
+pub struct CABatch<B: Backend> {
+    pub initial_states: Tensor<B, 4>,
+    pub expected_results: Tensor<B, 4>,
+}
+
+#[derive(Clone, Debug)]
+pub struct CAData {
+    pub state: Vec<Vec<Vec<f32>>>,    // height, width, visible channels
+    pub expected: Vec<Vec<Vec<f32>>>, // height, width, visible channels
+}
+
+impl<B: Backend> Batcher<CAData, CABatch<B>> for CABatcher<B> {
+    // The input data must consist of 2D images with **the same number of channels**.
+    fn batch(&self, data: Vec<CAData>) -> CABatch<B> {
+        let initial_states = data
+            .iter()
+            .map(|cadata| {
+                let (height, width) = (cadata.state.len(), cadata.state[0].len());
+                let channels = cadata.state[0][0].len();
+                let flattened_state = cadata
+                    .state
+                    .clone()
+                    .into_iter()
+                    .flatten()
+                    .flatten()
+                    .collect::<Vec<_>>();
+                TensorData::new(flattened_state, [height, width, channels])
+            })
+            .map(|tdata| Tensor::<B, 3>::from_data(tdata, &self.device))
+            .map(|tensor| tensor.unsqueeze_dim(0))
+            .collect();
+        let expected_results = data
+            .iter()
+            .map(|cadata| {
+                let (height, width) = (cadata.expected.len(), cadata.expected[0].len());
+                let channels = cadata.expected[0][0].len();
+                let flattened_expected = cadata
+                    .expected
+                    .clone()
+                    .into_iter()
+                    .flatten()
+                    .flatten()
+                    .collect::<Vec<_>>();
+                TensorData::new(flattened_expected, [height, width, channels])
+            })
+            .map(|tdata| Tensor::<B, 3>::from_data(tdata, &self.device))
+            .map(|tensor| tensor.unsqueeze_dim(0))
+            .collect();
+
+        // N H W C
+        let initial_states = Tensor::cat(initial_states, 0).to_device(&self.device);
+        let expected_results = Tensor::cat(expected_results, 0).to_device(&self.device);
+
+        // convert to N C H W
+        let initial_states = initial_states.permute([0, 3, 1, 2]);
+        let expected_results = expected_results.permute([0, 3, 1, 2]);
+        CABatch {
+            initial_states,
+            expected_results,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use burn::{
+        backend::{Wgpu, wgpu::WgpuDevice},
+        tensor::Shape,
+    };
+    type TestBackend = Wgpu<f32, i32>;
+
+    #[test]
+    fn test_batcher() {
+        let device = WgpuDevice::default();
+        let batcher: CABatcher<TestBackend> = CABatcher::new(device);
+        let data = vec![
+            CAData {
+                state: vec![vec![vec![0.0; 4]; 5]; 3],
+                expected: vec![vec![vec![1.0; 4]; 5]; 3],
+            },
+            CAData {
+                state: vec![vec![vec![2.0; 4]; 5]; 3],
+                expected: vec![vec![vec![3.0; 4]; 5]; 3],
+            },
+        ]; // 2D images with RGBA channels
+        let batch = batcher.batch(data);
+
+        // Output dimension should be in `NHWC` format
+        assert_eq!(batch.initial_states.shape(), Shape::new([2, 4, 3, 5]));
+        assert_eq!(batch.expected_results.shape(), Shape::new([2, 4, 3, 5]));
+
+        let binding = batch.expected_results.to_data();
+        let expected_batch: &[f32] = binding.as_slice().unwrap();
+        for i in 0..2 {
+            for j in 0..4 {
+                for k in 0..3 {
+                    for l in 0..5 {
+                        assert_eq!(
+                            expected_batch[i * 4 * 3 * 5 + j * 3 * 5 + k * 5 + l],
+                            ((i * 2) + 1) as f32
+                        );
+                    }
+                }
+            }
+        }
+
+        let data = vec![CAData {
+            state: vec![vec![vec![0.0; 4]; 5]; 3],
+            expected: vec![vec![vec![0., 1., 2., 3.]; 5]; 3],
+        }];
+        let batch = batcher.batch(data);
+
+        let binding = batch.expected_results.to_data();
+        let initial_batch: &[f32] = binding.as_slice().unwrap();
+        for i in 0..4 {
+            for j in 0..3 {
+                for k in 0..5 {
+                    assert_eq!(initial_batch[i * 3 * 5 + j * 5 + k], i as f32);
+                }
+            }
+        }
+    }
+}
---
+++ src/model/original_model.rs
@@ -0,0 +1,111 @@
+use burn::{
+    nn::{Linear, Relu},
+    prelude::*,
+    tensor::{
+        backend::AutodiffBackend,
+        module::{conv2d, max_pool2d},
+        ops::ConvOptions,
+    },
+    train::TrainStep,
+};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
+pub(crate) enum ModelType {
+    Growing,
+    Persistent,
+    Regenerating,
+}
+
+#[derive(Config, Debug)]
+pub(crate) struct OriginalModelConfig {
+    pub input_dim: usize,
+    pub output_dim: usize,
+
+    #[config(default = "16")]
+    pub state_channels: usize,
+    #[config(default = "16")]
+    pub target_padding: usize,
+    #[config(default = "40")]
+    pub target_size: usize,
+    #[config(default = "8")]
+    pub batch_size: usize,
+    #[config(default = "1024")]
+    pub pool_size: usize,
+    #[config(default = "0.5")]
+    pub cell_fire_rate: f64,
+
+    #[config(default = "0.1")]
+    pub alive_threshold: f64,
+
+    pub model_type: ModelType,
+}
+
+#[derive(Module, Debug)]
+pub(crate) struct OriginalModel<B: Backend> {
+    dense128: Linear<B>,
+    dense16: Linear<B>,
+    relu: Relu,
+
+    alive_threshold: f32,
+}
+
+impl OriginalModelConfig {
+    pub fn init<B: Backend>(&self, device: &B::Device) -> OriginalModel<B> {
+        let use_pattern_pool = match self.model_type {
+            ModelType::Growing => false,
+            ModelType::Persistent => true,
+            ModelType::Regenerating => true,
+        };
+        let damage_n = match self.model_type {
+            ModelType::Growing => 0,
+            ModelType::Persistent => 0,
+            ModelType::Regenerating => 3,
+        }; // number of patterns to damage in a batch
+        todo!()
+    }
+}
+
+const SOBEL_FILTER_X: [[f32; 3]; 3] = [[-1.0, 0.0, 1.0], [-2.0, 0.0, 2.0], [-1.0, 0.0, 1.0]];
+const SOBEL_FILTER_Y: [[f32; 3]; 3] = [[-1.0, -2.0, -1.0], [0.0, 0.0, 0.0], [1.0, 2.0, 1.0]];
+
+impl<B: Backend> OriginalModel<B> {
+    fn perceive(&self, states: Tensor<B, 4>) -> Tensor<B, 4> {
+        let device = states.device();
+        let conv_options: ConvOptions<2> = ConvOptions::new([1, 1], [1, 1], [1, 1], 1); // stride, padding, dilation, groups
+        let sobel_weights_x = Tensor::from_floats(SOBEL_FILTER_X, &device);
+        let sobel_weights_y = Tensor::from_floats(SOBEL_FILTER_Y, &device);
+        // partial derivative estimation with sobel filters
+        let grad_x = conv2d(states.clone(), sobel_weights_x, None, conv_options.clone());
+        let grad_y = conv2d(states.clone(), sobel_weights_y, None, conv_options);
+
+        Tensor::cat(vec![states, grad_x, grad_y], 1)
+    }
+    fn update(&self, perception: Tensor<B, 4>) -> Tensor<B, 4> {
+        let x = self.dense128.forward(perception);
+        let x = self.relu.forward(x);
+        self.dense16.forward(x)
+    }
+    fn alive_masking(&self, states: Tensor<B, 4>) -> Tensor<B, 4> {
+        let device = states.device();
+        let alpha_channels = states.clone().select(1, Tensor::from_ints([3], &device));
+        let adjacent_max_alpha = max_pool2d(alpha_channels, [3, 3], [1, 1], [1, 1], [1, 1]);
+
+        let alive_mask = adjacent_max_alpha
+            .greater_elem(self.alive_threshold)
+            .expand(states.clone().dims());
+
+        Tensor::zeros(states.dims(), &device).mask_where(alive_mask, states)
+    }
+    pub fn forward(&self, states: Tensor<B, 4>) -> Tensor<B, 4> {
+        let live_states = self.alive_masking(states);
+        let perception = self.perceive(live_states);
+
+        let updated_states = self.update(perception);
+
+        todo!("Perform stochastic cell update");
+    }
+}
+
+// impl <B:AutodiffBackend> TrainStep<> for OriginalModel<B> {
+// }
